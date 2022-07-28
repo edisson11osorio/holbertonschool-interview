577f242dfe26c639b72b001ce5f03b291a5f9562
edisson11osorio <osorioedisson08@gmail.com>
2022-07-28 16:34:10 -0500

    [✅]update code

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+contains the Pascal's Triangle function
+"""
+def pascal_triangle(n):
+    triangle = []
+    aux = 0
+    for i in range(n):
+        triangle.append([])
+    for i in range(n):
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                triangle[i].append(1)
+            else:
+                aux = triangle[i - 1][j] + triangle[i - 1][j - 1]
+                triangle[i].append(aux)
+    return triangle
